68b4fa7b489bb67f0ec97f6b1b5eadcfa90ef22c
Yves Mauron <yves@kayma.ch>
2020-09-22 13:28:23 +0200

    removed unnecessary files

--- prepare_labelling_data.bat
+++
@@ -1,3 +0,0 @@
-"C:\Program Files\R\R-3.6.1\bin\RScript.exe" scripts\preprocess\stage_edf_file.R --verbose
-"C:\Users\yvesm\Miniconda3\envs\rnn_anomaly\python.exe" scripts\preprocess\stage_to_curated.py
-"C:\Users\yvesm\Miniconda3\envs\rnn_anomaly\python.exe" scripts\preprocess\prepare_for_manual_labelling.py
